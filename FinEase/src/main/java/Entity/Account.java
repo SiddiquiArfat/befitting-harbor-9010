3e20816d66760cdf4f20d88243bf39c55fa61ec6
SiddiquiArfat <arfatsiddiqui070@gmail.com>
2023-05-04 23:59:25 +0530

    Day-2

---
+++ FinEase/src/main/java/Entity/Account.java
@@ -0,0 +1,109 @@
+package Entity;
+
+import java.util.List;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Account {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	String name;
+	String type;
+	String status;
+	String aadhar;
+	String accountNo;
+	
+	@OneToMany(mappedBy = "Account", cascade = CascadeType.ALL)
+	List<transaction> t;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "customer_id")
+	Customer c;
+
+	public Account() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Account(String name, String type, String status, String aadhar, String accountNo, Customer c) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.status = status;
+		this.aadhar = aadhar;
+		this.accountNo = accountNo;
+		this.c = c;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getAadhar() {
+		return aadhar;
+	}
+
+	public void setAadhar(String aadhar) {
+		this.aadhar = aadhar;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public List<transaction> getT() {
+		return t;
+	}
+
+	public void setT(List<transaction> t) {
+		this.t = t;
+	}
+
+	public Customer getC() {
+		return c;
+	}
+
+	public void setC(Customer c) {
+		this.c = c;
+	}
+	
+	
+	
+	
+	
+	
+}
